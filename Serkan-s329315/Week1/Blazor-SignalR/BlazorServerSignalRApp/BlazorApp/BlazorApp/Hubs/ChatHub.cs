12b46c56e9c183efbbaa2176396bd073fceddd5b
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-08-15 22:15:28 +0930

    projecr recreated in VS from VS code
    projecr recreated in VS from VS code

---
+++ Serkan-s329315/Week1/Blazor-SignalR/BlazorServerSignalRApp/BlazorApp/BlazorApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace BlazorApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
+
