1776a6b46378a7a2c25ab80320e03be7d48a2195
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-08-16 16:26:21 +0930

    fix bugs

--- Huy-s329531/Week1/BlazorToDoApp/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ Huy-s329531/Week1/BlazorToDoApp/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -96,7 +96,7 @@ using BlazorToDoApp.Shared;
     private int currentCount = 0;
 
     [Parameter]
-    public int IncrementAmount {get; set } = 1;
+    public int IncrementAmount {get; set; } = 1;
 
     private void IncrementCount()
     {
