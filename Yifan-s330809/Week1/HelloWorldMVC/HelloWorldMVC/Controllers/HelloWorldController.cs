e3134c65aad5106967227ee69426d2ec62b6bd2d
yifan-sco-20 <yifan.song@outlook.com.au>
2021-08-18 13:26:57 +0930

    Add a view

--- Yifan-s330809/Week1/HelloWorldMVC/HelloWorldMVC/Controllers/HelloWorldController.cs
+++ Yifan-s330809/Week1/HelloWorldMVC/HelloWorldMVC/Controllers/HelloWorldController.cs
@@ -14,9 +14,21 @@ namespace HelloWorldMVC.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
 
         // 
@@ -34,9 +46,9 @@ namespace HelloWorldMVC.Controllers
         //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         //}
 
-        public string Welcome(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
     }
 }
