eaa918f53770586ab6652517e72b47629fff6a90
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-08-24 09:16:31 +0930

    Updating code

--- Huy-s329531/Week2/MvcMovie/MvcMovie/Models/SeedData.cs
+++ Huy-s329531/Week2/MvcMovie/MvcMovie/Models/SeedData.cs
@@ -26,7 +26,9 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        
+                        Price = 7.99M,
+                        Rating = "R",
                     },
 
                     new Movie
@@ -34,6 +36,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -42,6 +45,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -50,6 +54,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
