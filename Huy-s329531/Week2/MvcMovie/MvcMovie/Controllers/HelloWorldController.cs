12b08f51407d1b245fdfce3d98948ef70d740d8e
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-08-23 18:37:46 +0930

    Created Movies Class

--- Huy-s329531/Week2/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ Huy-s329531/Week2/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
@@ -19,9 +20,10 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            //return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
