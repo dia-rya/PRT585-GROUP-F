14442fb14cf759e715747cd0ede23d36a45ae94d
yifan-sco-20 <yifan.song@outlook.com.au>
2021-08-25 01:08:06 +0930

    Created razor view files for Index method and Admin method, changed layout of web app, updated Admin method to pass data from controller to view

--- Yifan-s330809/Week2/MVCMovie/MVCMovie/Controllers/MoviesController.cs
+++ Yifan-s330809/Week2/MVCMovie/MVCMovie/Controllers/MoviesController.cs
@@ -11,33 +11,53 @@ namespace MVCMovie.Controllers
 {
     public class MoviesController : Controller
     {
+        /*GET: /Movie/
+         * Returns a string
+         */
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
         // GET: /Movie/
-        public string Index()
+        // Action Method
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            // Calls controller's View Method. 
+            return View();
         }
 
-        // GET: /Movie/Admin/
-        // Requires using System.Text.Encodings.Web;
-        // Access URL: https://localhost:5001/Movies/Admin?username=Yifan&userID=7
+        /* GET: /Movie/Admin/
+         * Requires using System.Text.Encodings.Web;
+         * Access URL: https://localhost:5001/Movies/Admin?username=Yifan&userID=7
+         * */
         //public string Admin(string username, int userID = 1)
         //{
         //    return HtmlEncoder.Default.Encode($"Hello, {username}, your user ID is: {userID}");
         //}
 
+        /* GET: /Movie/Admin/
+         * Requires using System.Text.Encodings.Web;
+         * Changes the parameter matching to default routing logic parameter
+         * Access URL: https://localhost:5001/Movies/Admin/7?username=Yifan
+         * Returns a string
+         */
+        //public string Admin(string username, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello, {username}, your user ID is: {ID}");
+        //}
+
+
         // GET: /Movie/Admin/
-        // Requires using System.Text.Encodings.Web;
-        // Changes the parameter matching to default routing logic parameter
-        // Access URL: https://localhost:5001/Movies/Admin/7?username=Yifan
-        public string Admin(string username, int ID = 1)
+        // Action Method
+        public IActionResult Admin(string username, int userID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello, {username}, your user ID is: {ID}");
+            ViewData["Username"] = "Username:" + username;
+            ViewData["UserID"] = "User ID: "+ userID;
+
+            return View();
         }
 
-        //// GET: /<controller>/
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+
     }
 }
