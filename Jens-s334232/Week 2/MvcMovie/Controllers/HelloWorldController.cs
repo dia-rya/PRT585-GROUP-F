65b60acdd6bb654484d66e752341d42aee602e41
mer <51684390+pdflu@users.noreply.github.com>
2021-08-25 08:16:25 +0930

    tried to fix namespace

--- Jens-s334232/Week 2/MvcMovie/Controllers/HelloWorldController.cs
+++ Jens-s334232/Week 2/MvcMovie/Controllers/HelloWorldController.cs
@@ -12,14 +12,14 @@ namespace MvcMovie.Controllers
         {
             return View();
         }
-            public IActionResult Welcome(string name, int numTimes = 1)
-            {
-                ViewData["Message"] = "Hello " + name;
-                ViewData["NumTimes"] = numTimes;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
-                return View();
-            }
+            return View();
         }
+        
 
         //  
         // GET: /HelloWorld/Welcome/ 
