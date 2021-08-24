1f8bd68f0b79d138d9f704ccab9c118265811e51
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-08-24 23:40:30 +0930

    Updating code

---
+++ Huy-s329531/Week2/MvcTheater/MvcTheater/Data/MvcTheaterContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcTheater.Models;
+
+namespace MvcTheater.Data
+{
+    public class MvcTheaterContext : DbContext
+    {
+        public MvcTheaterContext (DbContextOptions<MvcTheaterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcTheater.Models.Movie> Movie { get; set; }
+    }
+}
