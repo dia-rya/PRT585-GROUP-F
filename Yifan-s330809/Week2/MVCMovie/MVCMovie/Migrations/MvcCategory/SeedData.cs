5ad95dbabfd8ede706d710d5564f94fcaf587ba1
yifan-sco-20 <yifan.song@outlook.com.au>
2021-08-25 04:46:11 +0930

    Updated the databases to the latest migration

---
+++ Yifan-s330809/Week2/MVCMovie/MVCMovie/Migrations/MvcCategory/SeedData.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVCMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+
+
+            using (var context = new MvcCategoryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcCategoryContext>>()))
+            {
+                // Look for any movies.
+                if (context.Category.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Category.AddRange(
+                    new Category
+                    {
+                        Name = "Comedy",
+                        Code = "C01"
+
+                    },
+
+                    new Category
+                    {
+                        Name = "Action",
+                        Code = "A01"
+
+                    },
+
+                    new Category
+                    {
+                        Name = "Drama",
+                        Code = "D01"
+
+                    },
+
+                    new Category
+                    {
+                        Name = "Horror",
+                        Code = "H01"
+
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+
+
+}
