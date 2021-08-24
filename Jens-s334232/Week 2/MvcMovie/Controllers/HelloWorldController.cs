ffc29fe14e3db87079461bffca923028607799f6
mer <51684390+pdflu@users.noreply.github.com>
2021-08-24 23:25:01 +0930

    Stuck at syntax

---
+++ Jens-s334232/Week 2/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+            public IActionResult Welcome(string name, int numTimes = 1)
+            {
+                ViewData["Message"] = "Hello " + name;
+                ViewData["NumTimes"] = numTimes;
+
+                return View();
+            }
+        }
+
+        //  
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
