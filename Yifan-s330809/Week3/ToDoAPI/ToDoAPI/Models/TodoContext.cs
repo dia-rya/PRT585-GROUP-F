27dfb32ede40857e1a08087b932577b66b93c985
yifan-sco-20 <yifan.song@outlook.com.au>
2021-09-01 13:47:20 +0930

    Bug fixed

--- Yifan-s330809/Week3/ToDoAPI/ToDoAPI/Models/TodoContext.cs
+++ Yifan-s330809/Week3/ToDoAPI/ToDoAPI/Models/TodoContext.cs
@@ -1,16 +1,15 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace ToDoAPI.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
-        public class TodoContext : DbContext
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
         {
-            public TodoContext(DbContextOptions<TodoContext> options)
-                : base(options)
-            {
-            }
-
-            public DbSet<TodoItem> TodoItems { get; set; }
         }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
