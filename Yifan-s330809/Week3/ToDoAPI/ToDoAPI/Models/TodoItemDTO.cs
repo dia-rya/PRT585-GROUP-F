a2f1539023b2d242491403183805c6964275c161
yifan-sco-20 <yifan.song@outlook.com.au>
2021-09-01 14:50:28 +0930

    Created DTO Model

--- Yifan-s330809/Week3/ToDoAPI/ToDoAPI/Models/TodoItemDTO.cs
+++ Yifan-s330809/Week3/ToDoAPI/ToDoAPI/Models/TodoItemDTO.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 namespace ToDoAPI.Models
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-        public string Secret { get; set; }
-
     }
-
 }
