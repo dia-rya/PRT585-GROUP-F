eb926dd56bd7f1deb2b49300698be2ea8cbae063
yifan-sco-20 <yifan.song@outlook.com.au>
2021-09-01 13:36:54 +0930

    Add and register a database

---
+++ Yifan-s330809/Week3/ToDoAPI/ToDoAPI/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ToDoAPI.Models
+{
+    public class TodoContext
+    {
+        public class TodoContext : DbContext
+        {
+            public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; }
+        }
+    }
+}
