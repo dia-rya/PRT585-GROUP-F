d4688a95331b0a776157150fe8e41cad5df4cd6a
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-09-06 17:08:28 +0930

    Updating code

---
+++ NgoTruongHuyTRUONG-s329531/Week4/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
