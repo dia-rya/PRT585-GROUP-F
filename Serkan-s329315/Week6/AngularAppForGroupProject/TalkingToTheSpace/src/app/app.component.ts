85f3ca5232faa3849e79fa4fc4ec8e5d355c83ee
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-09-23 00:11:26 +0930

    Main page Cleared.
    Main page Cleared from template.

--- Serkan-s329315/Week6/AngularAppForGroupProject/TalkingToTheSpace/src/app/app.component.ts
+++ Serkan-s329315/Week6/AngularAppForGroupProject/TalkingToTheSpace/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TalkingToTheSpace';
+  title = 'Talking To The Space';
 }
