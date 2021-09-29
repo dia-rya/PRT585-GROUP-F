64eeb30722ca4fdfe89f8da947ee4708c2fb906b
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-09-29 12:37:03 +0930

    3 tier app.
    V1

--- Serkan-s329315/Week6/AngularAppForGroupProject/TalkingToTheSpace/src/app/home/home.component.ts
+++ Serkan-s329315/Week6/AngularAppForGroupProject/TalkingToTheSpace/src/app/home/home.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, state, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  isDisabled = false;
+  isOpen = false;
+  toggleAnimations() {
+    this.isDisabled = !this.isDisabled;
+  }
 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
   ngOnInit(): void {
   }
 
