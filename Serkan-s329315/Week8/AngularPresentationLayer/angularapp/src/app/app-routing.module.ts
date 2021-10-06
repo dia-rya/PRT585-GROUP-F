8272aadde95b75a58cedd68d2323e8a0b680680e
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-10-06 14:36:03 +0930

    Add home and submit application component

--- Serkan-s329315/Week8/AngularPresentationLayer/angularapp/src/app/app-routing.module.ts
+++ Serkan-s329315/Week8/AngularPresentationLayer/angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubmitApplicationComponent } from './submit-application/submit-application.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'apply', component: SubmitApplicationComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
