fe7ccdbaee8aebfc8dd3253de2fe2a5cc5438ccb
huytntauscdu <62043433+huytntauscdu@users.noreply.github.com>
2021-10-27 01:21:04 +0930

    fix bugs

--- NgoTruongHuyTRUONG-s329531/week10/smart-tablev2/src/app/app.module.ts
+++ NgoTruongHuyTRUONG-s329531/week10/smart-tablev2/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
